cfde4c82f47d3f6168a3f7b62202cf9c46bc2187
Nikita <nikita.evgrafov@inbox.ru>
2022-09-01 17:01:15 +0500

    add bullety

--- src/components/Posts/Post.jsx
+++ src/components/Posts/Post.jsx
@@ -2,6 +2,8 @@ import axios from 'axios';
 import React, { useState, useEffect } from 'react';
 import styles from './Post.module.scss';
 
+import usePagination from '../../hooks/usePagination';
+
 const Post = () => {
   const [posts, setPosts] = useState([]);
 
@@ -18,7 +20,17 @@ const Post = () => {
 
   }, []);
 
-
+  const {
+    firstContentIndex,
+    lastContentIndex,
+    nextPage,
+    prevPage,
+    page,
+    totalPages
+  } = usePagination({
+    contentPerPage: 10,
+    count: posts.length,
+  });
 
   return (
     <div className={styles.wrapper__content}>
@@ -26,7 +38,9 @@ const Post = () => {
         <h1>Посты</h1>
       </div>
       <ul className={styles.posts__list}>
-        {posts.map((item, index) => (
+        {posts
+        .slice(firstContentIndex, lastContentIndex)
+        .map((item, index) => (
           <li key={index} className={styles.post__item}>
             <div className="title__post">
               <h4>{item.title}</h4>
@@ -37,8 +51,23 @@ const Post = () => {
             <a href="/">Читать полностью</a>
           </li>
         ))}
-
       </ul>
+      <div className={styles.pagination}>
+        <span className={styles.count__page}>{page}/{totalPages}</span>
+        <div className={styles.bullets__page}>
+          <button
+          onClick={() => prevPage()}
+          className={styles.prev__page}
+          >prev</button>
+          {[...Array(totalPages).keys()].map((num) => (
+              <span className={`${styles.bullet} ${(num + 1) === page ? styles.bullet__active : " "}`}>{num + 1}</span>
+          ))}
+          <button 
+          className={styles.next__page}
+          onClick={() => nextPage()}
+          >next</button>
+        </div>
+      </div>
     </div>
 
   )
